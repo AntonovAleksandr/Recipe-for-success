b2b119730020311aa7cc02c7b40ffd626beda1de
AntonovAleksandr <zzzz1917@bk.ru>
2022-01-06 19:48:26 +0300

    repositories update

--- src/main/java/com/example/tdddevelopment/data/repository/IngredientRepository.java
+++ src/main/java/com/example/tdddevelopment/data/repository/IngredientRepository.java
@@ -3,9 +3,11 @@ package com.example.tdddevelopment.data.repository;
 import com.example.tdddevelopment.data.entity.Ingredient;
 import com.example.tdddevelopment.data.entity.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IngredientRepository extends PagingAndSortingRepository<Ingredient, Long> {
     Optional<Ingredient> findById(Long id);
 }
--- src/main/java/com/example/tdddevelopment/data/repository/StorageElementRepository.java
+++ src/main/java/com/example/tdddevelopment/data/repository/StorageElementRepository.java
@@ -3,7 +3,11 @@ package com.example.tdddevelopment.data.repository;
 import com.example.tdddevelopment.data.entity.Step;
 import com.example.tdddevelopment.data.entity.StorageElement;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StorageElementRepository extends PagingAndSortingRepository<StorageElement, Long> {
-
+    List<StorageElement> findAll();
 }
