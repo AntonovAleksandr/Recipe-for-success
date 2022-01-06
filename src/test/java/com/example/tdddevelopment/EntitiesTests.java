7854d41c3be52af7314f17c089580d1a316842a8
AntonovAleksandr <zzzz1917@bk.ru>
2022-01-06 19:46:53 +0300

    Tests loading

--- src/test/java/com/example/tdddevelopment/EntitiesTests.java
+++ src/test/java/com/example/tdddevelopment/EntitiesTests.java
@@ -6,12 +6,16 @@ import com.example.tdddevelopment.data.entity.Ingredient;
 import com.example.tdddevelopment.data.entity.Product;
 import com.example.tdddevelopment.data.entity.Step;
 import com.example.tdddevelopment.data.enums.Measure;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.fail;
 
 @SpringBootTest
 public class EntitiesTests {
@@ -29,18 +33,17 @@ public class EntitiesTests {
     @Test
     void StepConstructorNegativeTest(){
         IngredientDto ingredient = new IngredientDto(-2.0, Measure.KILOGRAM, new ProductDto());
-        assert ingredient.getQuantity() > 0;
-        assert ingredient.getMeasure() != null;
+        Assertions.fail();
     }
-    @Test
-    void ProductConstructorTest(){
-        ProductDto product = new ProductDto("Cola", List.of(new StepDto()));
-        assert !product.getRecipe().isEmpty();
-    }
-    @Test
-    void ProductConstructorNegativeTest(){
-        ProductDto product = new ProductDto("Cola", new ArrayList<>());
-        assert product.getRecipe().isEmpty();
-    }
+//    @Test
+//    void ProductConstructorTest(){
+//        ProductDto product = new ProductDto("Cola", List.of(new StepDto()));
+//        assert !product.getRecipe().isEmpty();
+//    }
+//    @Test
+//    void ProductConstructorNegativeTest(){
+//        ProductDto product = new ProductDto("Cola", new ArrayList<>());
+//        assert product.getRecipe().isEmpty();
+//    }
 
 }
--- src/test/java/com/example/tdddevelopment/TddDevelopmentApplicationTests.java
+++ src/test/java/com/example/tdddevelopment/TddDevelopmentApplicationTests.java
@@ -1,17 +1,32 @@
 package com.example.tdddevelopment;
 
+import com.example.tdddevelopment.data.dto.ProductDto;
+import com.example.tdddevelopment.service.RecipeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class TddDevelopmentApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//
-//
-//
-//
-//}
+import java.util.List;
+
+@SpringBootTest
+class TddDevelopmentApplicationTests {
+    @Autowired
+    private RecipeService recipeService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void favoriteProductTest() {
+        List<ProductDto> productDtos = recipeService.getFavouriteProducts();
+        for (ProductDto productDto : productDtos) {
+            assert productDto.getIsFavorite();
+        }
+    }
+
+
+
+
+
+}
