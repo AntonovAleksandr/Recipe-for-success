9e3cf545617564ad3fcc74e4e78796aee15c956f
AntonovAleksandr <zzzz1917@bk.ru>
2022-01-10 05:13:01 +0300

    Filtering service

--- src/main/java/com/example/tdddevelopment/service/FilteringService.java
+++ src/main/java/com/example/tdddevelopment/service/FilteringService.java
@@ -1,10 +1,13 @@
 package com.example.tdddevelopment.service;
 
+import com.example.tdddevelopment.data.dto.ProductDto;
 import com.example.tdddevelopment.data.entity.Product;
 
 import java.util.List;
 
 public interface FilteringService {
-    List<Product> priceFilter(double min, double max);
-    List<Product> ingredientFilter(List<String> names);
+    List<ProductDto> priceFilter(List<ProductDto> products, double min, double max);
+    List<ProductDto> ingredientFilter(List<ProductDto> products, List<String> ingredientsTitles);
+    List<ProductDto> titleFilter(List<ProductDto> products, String title, boolean fullCoincidence);
+
 }
--- src/main/java/com/example/tdddevelopment/service/impl/FilteringServiceImpl.java
+++ src/main/java/com/example/tdddevelopment/service/impl/FilteringServiceImpl.java
@@ -1,20 +1,40 @@
 package com.example.tdddevelopment.service.impl;
 
-import com.example.tdddevelopment.data.entity.Product;
+import com.example.tdddevelopment.data.dto.ProductDto;
 import com.example.tdddevelopment.service.FilteringService;
-import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-@Service
+import java.util.stream.Collectors;
+
 public class FilteringServiceImpl implements FilteringService {
 
+    private boolean between(ProductDto productDto, double min, double max) {
+        if (productDto == null) return false;
+        return !(productDto.getMaxPrice() < min) || !(productDto.getMinPrice() > max);
+    }
+
+
     @Override
-    public List<Product> priceFilter(double min, double max) {
-        return null;
+    public List<ProductDto> priceFilter(List<ProductDto> products, double min, double max) {
+        if (products == null || products.isEmpty() || min < max || max < 0) return null;
+        return products.stream().filter(e -> between(e, min, max)).collect(Collectors.toList());
     }
 
     @Override
-    public List<Product> ingredientFilter(List<String> names) {
+    public List<ProductDto> ingredientFilter(List<ProductDto> products, List<String> ingredientsTitles) {
+        if (products == null || products.isEmpty() || ingredientsTitles == null || ingredientsTitles.isEmpty())
+            return null;
         return null;
+        //return products.stream().filter(e -> e.getRecipe().size() ).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProductDto> titleFilter(List<ProductDto> products, String title, boolean fullCoincidence) {
+        if (products == null || products.isEmpty() || title == null) return null;
+        if (title.isEmpty()) return products;
+        if (fullCoincidence)
+            return products.stream().filter(e -> e.getTitle().equals(title)).collect(Collectors.toList());
+        return products.stream().filter(e -> e.getTitle().contains(title) || title.contains(e.getTitle())).collect(Collectors.toList());
     }
 }
